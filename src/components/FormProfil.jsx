da1e834cb2a25053f3ce7eda39cfdad7a5042388
Huynh <raphaelle.huynh@outlook.fr>
2023-09-28 00:58:03 +0200

    feat : Page FormProfil

--- src/components/FormProfil.jsx
+++ src/components/FormProfil.jsx
@@ -1,40 +1,48 @@
-import { useEffect, useState } from "react";
+import { useState , useRef } from "react";
 import { useParams } from "react-router-dom";
 import NavBar from "./NavBar";
 
 function FromProfil(){
-    const [user, setUser] = useState([]);
-    const [networks, setNetwork] = useState([]);
-    const [categories, setCategory] = useState([]);
-    const [selectedFile, setSelectedFile] = useState(null);
+    const id = useParams();
+    const [user,setUser] = useState({
+        id : id.profil,
+        lastname : null,
+        firstname : null,
+        description : null,
+        mail : null,
+        url_portfolio : null,
+    });
+    const form = useRef(null);
+
+    const submit = () => {
+        const data = new FormData(form.current)
+        data.append("id",user.id);
+        fetch('http://127.0.0.1:8000/members', {
+            type: "PATCH", body: data})
+            .then(res => res.json())
+            .then(json => setUser(json.user))
+        for( const [key,value] of data){
+            console.log(key + " : " +value);
+        }
+    }
 
     return (
-        <main>
-            <NavBar/>
-            <section className="flex flex-col items-center justify-center w-full min-h-screen px-5">
-                <article className="flex flex-col">
-                <h2 className="text-4xl md:text-5xl mb-5 text-beige bg-black uppercase py-1 px-1 top-0">Modifier profil</h2>
-                <form className="flex flex-col gap-2 text-gray-800">
-                    <label>Pseudo :</label>
-                    <input type="text" name="username" className="border-2"/>
-                    <label>Prénom :</label>
-                    <input type="text" name="firstname" className="border-2"/>
-                    <label>Nom :</label>
-                    <input type="text" name="lastname" className="border-2"/>
-                    <label>Description :</label>
-                    <textarea name="description" className="border-2"/>
-                    <label>Mail :</label>
-                    <input type="text" name="mail" className="border-2"/>
-                    <label>Url Portfolio :</label>
-                    <input type="text" name="url_portfolio" className="border-2"/>
-                    <label>Image du Portfolio :</label>
-                    <input type="file" value={selectedFile} onChange={(e) => setSelectedFile(e.target.files(0))}/>
-                    <img src="" height="200" alt="Prévisualisation"></img>
-                    <input type="submit" value="Confirmer" className="bg-black px-2 py-2 text-beige hover:bg-slate-800 cursor-pointer"/>
-                </form>
-                </article>
-            </section>
-        </main>
+        <>
+        <NavBar/>
+        <form ref={form} onSubmit={submit} className="flex flex-col p-20">
+            <label htmlFor="lastname">Nom :</label>
+            <input type="text" name="lastname" defaultValue={user.lastname}/>
+            <label htmlFor="firstname">Prénom :</label>
+            <input type="text" name="firstname" defaultValue={user.firstname}/>
+            <label htmlFor="description">Description :</label>
+            <input type="text" name="description" defaultValue={user.description}/>
+            <label htmlFor="mail">Email :</label>
+            <input type="mail" name="mail" defaultValue={user.mail}/> 
+            <label htmlFor="url_portfolio">Url du portfolio :</label>
+            <input type="text" name="url_portfolio" defaultValue={user.url_portfolio}/>
+            <input type="submit" name="update_profil" value="Valider"/>
+        </form>
+        </>
     )
 }
 
