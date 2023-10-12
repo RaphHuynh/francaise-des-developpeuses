2a16ab8a27fd9991c9bd6fce109425edd6b44138
Huynh <raphaelle.huynh@outlook.fr>
2023-10-13 01:32:03 +0200

    feat : patch image portfolio in FormProfil

--- src/components/FormProfil.jsx
+++ src/components/FormProfil.jsx
@@ -12,26 +12,66 @@ function FromProfil() {
         mail: null,
         url_portfolio: null,
     });
+    const [isImageSelected, setIsImageSelected] = useState(false);
+    const [file, setFile] = useState(null);
     const form = useRef(null);
 
     const submit = () => {
         const data = new FormData(form.current)
         data.append("id", user.id);
         const object = {};
-        for (const [key, value] of data) {
-            console.log(key + " : " + value);
-        }
         data.forEach(function (value, key) {
             object[key] = value;
         })
-        console.log(JSON.stringify(object));
-        debugger;
         fetch('http://127.0.0.1:8000/member', {
             method: "PATCH", headers: { "content-type": "application/json" }, body: JSON.stringify(object)
         })
             .then(res => res.json())
             .then(json => setUser(json.user))
     }
+
+    const handleFileChange = (event) => {
+        const selectedFile = event.target.files[0];
+        setFile(selectedFile);
+        setIsImageSelected(!!selectedFile);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+    
+        if (!file) {
+            alert('Veuillez sélectionner un fichier image.');
+            return;
+        }
+    
+        const formData = new FormData();
+        const object = [];
+        formData.append('file', file, file.name);
+        formData.append('id_member', id.profil);
+
+        formData.forEach(function (value, key) {
+            object[key] = value;
+        })
+
+        console.log(object);
+
+        debugger;
+    
+        try {
+            const response = await fetch('http://127.0.0.1:8000/member/image_portfolio?id_member='+id.profil, {
+                method: 'PATCH',
+                body: formData
+            });
+    
+            if (response.status === 200) {
+                alert("L'image a été téléchargée avec succès.");
+            } else {
+                alert('Une erreur s\'est produite lors de l\'envoi de l\'image.');
+            }
+        } catch (error) {
+            console.error('Erreur lors de la requête :', error);
+        }
+    };    
 
     return (
         <>
@@ -61,10 +101,15 @@ function FromProfil() {
                             <input type="mail" name="mail" defaultValue={user.mail} className="bg-transparent border-b border-black focus:border-slate-400 p-1 focus:ring-transparent focus:outline-none focus:placeholder:text-slate-400 placeholder:text-slate-500" placeholder="example@example.com" />
                             <label htmlFor="url_portfolio" className="uppercase">Url du portfolio :</label>
                             <input type="text" name="url_portfolio" defaultValue={user.url_portfolio} className="bg-transparent border-b border-black focus:border-slate-400 p-1 focus:ring-transparent focus:outline-none focus:placeholder:text-slate-400 placeholder:text-slate-500" placeholder="www.example.com" />
-                            <input type="submit" name="update_profil" value="Valider" className="border-2 border-black uppercase py-2 px-3 bg-black text-beige hover:scale-105 transition delay-75 mt-4 w-full"/>
+                            <input type="submit" name="update_profil" value="Valider" className="border-2 border-black uppercase py-2 px-3 bg-black text-beige hover:scale-105 transition delay-75 mt-4 w-full" />
                         </form>
                     </article>
                     <article className="flex flex-col justify-center w-1/2 px-20 pt-20">
+                        <h1>Envoyer une image</h1>
+                        <form onSubmit={handleSubmit} className="flex flex-col">
+                            <input type="file" onChange={handleFileChange} accept="image/jpeg, image/png" />
+                            <input type="submit" value="Envoyer" className="hover:cursor-pointer" />
+                        </form>
                     </article>
                 </section>
             </section>
