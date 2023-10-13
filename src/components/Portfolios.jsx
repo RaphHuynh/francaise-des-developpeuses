e99e504b1e1222b2b37dff233dbda20b6a477d98
Huynh <raphaelle.huynh@outlook.fr>
2023-10-13 04:23:44 +0200

    feat : pagination -> Portfolios

--- src/components/Portfolios.jsx
+++ src/components/Portfolios.jsx
@@ -1,23 +1,54 @@
+import React, { useState } from "react";
 import NavBar from "./NavBar";
 import Card from "./component/Card";
 
-function Portfolios(){
+function Portfolios() {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1); // Mise à jour du nombre total de pages
+
+    const handlePageChange = (newPage) => {
+        if (newPage >= 1 && newPage <= totalPages) {
+            setCurrentPage(newPage);
+        } else if (newPage > totalPages) {
+            setCurrentPage(1); // Revenir à la première page si on atteint la dernière.
+        }
+    };
+
     return (
         <main>
-            <NavBar/>
-            <section className="flex items-center w-full min-h-screen px-5 md:px-20 pt-20 md:pt-32 pb-10">
+            <NavBar />
+            <section className="flex w-full min-h-screen px-5 md:px-20 pt-20 md:pt-32 pb-10">
                 <div className="flex flex-col w-full">
-                    <article className="md:flex md:items-center mb-5 md:mb-20 w-full pb-5 md:pb-10 border-b gap-4">
+                    <article className="md:flex md:items-center mb-5 md:mb-10 w-full pb-5 md:pb-10 border-b gap-4">
                         <h1 className="text-2xl lg:text-4xl xl:text-6xl lg:w-1/2 uppercase">Liste des portfolios</h1>
                         <p className="md:w-1/2 text-sm lg:text-lg">Liste des profils acceptés. Il est possible de tomber sur des profils francophones hors France. Ses profils ont été accepté dans le cadre où les personnes concernées s'investissent dans la communauté francophone où recherche un emploi en France.</p>
                     </article>
                     <article className="flex items-center justify-center w-full">
-                        <Card/>
+                        <Card
+                            currentPage={currentPage}
+                            // Mise à jour du nombre total de pages
+                            updateTotalPages={(total) => setTotalPages(total)}
+                        />
                     </article>
+                    <div className="flex justify-center mt-5 gap-4">
+                        <button
+                            onClick={() => handlePageChange(currentPage - 1)}
+                            disabled={currentPage === 1}
+                            className="text-beige bg-black px-3 py-2 text-lg disabled:bg-slate-700 hover:scale-110 disabled:hover:scale-100 transition delay-75"
+                        >
+                            {"<"}
+                        </button>
+                        <button
+                            onClick={() => handlePageChange(currentPage + 1)}
+                            className="text-beige bg-black px-3 py-2 text-lg hover:scale-110 transition delay-75"
+                        >
+                            {">"}
+                        </button>
+                    </div>
                 </div>
             </section>
         </main>
-    )
+    );
 }
 
 export default Portfolios;
--- src/components/component/Card.jsx
+++ src/components/component/Card.jsx
@@ -2,23 +2,30 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import api from "../api/api_get_all";
 
-function Card(){
+function Card({ currentPage, onPageChange, updateTotalPages }) {
     const [members, setMembers] = useState([]);
-    const [isMounted, setIsMounted] = useState(false);
+    const profilesPerPage = 8;
 
     useEffect(() => {
-        !isMounted &&
         api.getMembers().then((json) => {
             setMembers(json);
-            setIsMounted(true);
+
+            // Calculez le nombre total de pages en fonction du nombre de profils
+            const total = Math.ceil(json.length / profilesPerPage);
+            updateTotalPages(total);
         });
-    }, [isMounted]);
+    }, [updateTotalPages]);
+
+    // Calculez l'index de début et de fin pour les profils sur la page actuelle
+    const indexOfLastProfile = currentPage * profilesPerPage;
+    const indexOfFirstProfile = indexOfLastProfile - profilesPerPage;
+    const currentProfiles = members.slice(indexOfFirstProfile, indexOfLastProfile);
 
     return (
-        <article className=": sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6">
-            {members.map((member) =>(
-                <Link to={`${member.id_member}`} key={member.id_member} className="transition delay-75  md:border-b h-min hover:scale-105 duration-50 md:border">
-                    <img src={"http://127.0.0.1:8000/member/image_portfolio_by_id?id_member="+member.id_member} alt={`${member.username}`} className="object-cover w-full h-56 hover:contrast-125 duration-200 md:border-b-2"></img>
+        <article className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4 md:gap-6">
+            {currentProfiles.map((member) => (
+                <Link to={`${member.id_member}`} key={member.id_member} className="transition delay-75 border h-min hover:scale-105 duration-50">
+                    <img src={"http://127.0.0.1:8000/member/image_portfolio_by_id?id_member=" + member.id_member} alt={member.username} className="object-cover w-full h-56 hover:contrast-125 duration-200"></img>
                     <aside className="pt-2 md:p-2 md:h-1/3">
                         <h1 className="uppercase text-beige bg-black py-1 px-1 mb-2 w-min">{member.username}</h1>
                         <p className="text-right">{member.category_name}</p>
@@ -26,7 +33,7 @@ function Card(){
                 </Link>
             ))}
         </article>
-    )
+    );
 }
 
 export default Card;
